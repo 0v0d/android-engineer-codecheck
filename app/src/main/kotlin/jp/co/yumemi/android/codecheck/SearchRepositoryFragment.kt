21ca9fa5c2bd840d5a19db234ae8f91c4aa1dd4a
energ <energyremtu@gmail.com>
2024-03-14 00:53:21 +0900

    インポートを明確化した

--- app/src/main/kotlin/jp/co/yumemi/android/codecheck/SearchRepositoryFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/codecheck/SearchRepositoryFragment.kt
@@ -11,9 +11,12 @@ import android.view.inputmethod.EditorInfo
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.*
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
 import jp.co.yumemi.android.codecheck.databinding.FragmentSearchRepositoryBinding
-
 
 class SearchRepositoryFragment : Fragment(R.layout.fragment_search_repository) {
 
