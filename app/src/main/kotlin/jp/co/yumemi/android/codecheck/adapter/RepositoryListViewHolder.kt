f7df2e05a0fc222382b0e57829b3b3fff0242969
energ <energyremtu@gmail.com>
2024-03-16 21:21:10 +0900

    DataBindingを追加し、xml内でtextを代入するように変更

--- app/src/main/kotlin/jp/co/yumemi/android/codecheck/adapter/RepositoryListViewHolder.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/codecheck/adapter/RepositoryListViewHolder.kt
@@ -1,14 +1,14 @@
 package jp.co.yumemi.android.codecheck.adapter
 
 import androidx.recyclerview.widget.RecyclerView
-import jp.co.yumemi.android.codecheck.databinding.LayoutItemBinding
+import jp.co.yumemi.android.codecheck.databinding.LayoutRepositoryListItemBinding
 import jp.co.yumemi.android.codecheck.model.RepositoryItem
 
 /**
  * リポジトリリストビューホルダー
  * @param binding レイアウトバインディング
  */
-class RepositoryListViewHolder(private val binding: LayoutItemBinding) :
+class RepositoryListViewHolder(private val binding: LayoutRepositoryListItemBinding) :
     RecyclerView.ViewHolder(binding.root) {
 
     /**
@@ -16,8 +16,10 @@ class RepositoryListViewHolder(private val binding: LayoutItemBinding) :
      * @param item リポジトリアイテム
      * @param listener リポジトリクリックリスナー
      */
-    fun bind(item: RepositoryItem, listener: RepositoryListAdapter.OnItemClickListener) {
-        binding.repositoryNameView.text = item.name
-        binding.root.setOnClickListener { listener.itemClick(item) }
+    fun bind(item: RepositoryItem, listener: (RepositoryItem) -> Unit) {
+        binding.repositoryItem = item
+        binding.root.setOnClickListener {
+            listener(item)
+        }
     }
 }
--- app/src/main/kotlin/jp/co/yumemi/android/codecheck/view/RepositoryDetailFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/codecheck/view/RepositoryDetailFragment.kt
@@ -8,7 +8,6 @@ import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
-import coil.load
 import jp.co.yumemi.android.codecheck.R
 import jp.co.yumemi.android.codecheck.databinding.FragmentRepositoryDetailBinding
 
@@ -27,15 +26,6 @@ class RepositoryDetailFragment : Fragment(R.layout.fragment_repository_detail) {
 
         Log.d("検索した日時", args.lastSearchDate)
         val binding = FragmentRepositoryDetailBinding.bind(view)
-        val repositoryItem = args.repositoryItem
-
-        binding.ownerIconView.load(repositoryItem.ownerIconUrl)
-        binding.nameView.text = repositoryItem.name
-        binding.languageView.text = getString(R.string.written_language, repositoryItem.language)
-        binding.starsView.text = getString(R.string.stars, repositoryItem.stargazersCount)
-        binding.watchersView.text = getString(R.string.watchers, repositoryItem.watchersCount)
-        binding.forksView.text = getString(R.string.forks, repositoryItem.forksCount)
-        binding.openIssuesView.text =
-            getString(R.string.open_issues, repositoryItem.openIssuesCount)
+        binding.repositoryItem = args.repositoryItem
     }
 }
