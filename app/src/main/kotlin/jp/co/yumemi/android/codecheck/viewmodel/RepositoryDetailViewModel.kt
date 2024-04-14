7865631c96f7adc440aa59df04dc4f2fd60cd728
0v0d <energyremtu@gmail.com>
2024-04-14 21:38:08 +0900

    LiveDataからStateFlowに変更

--- app/src/main/kotlin/jp/co/yumemi/android/codecheck/viewmodel/RepositoryDetailViewModel.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/codecheck/viewmodel/RepositoryDetailViewModel.kt
@@ -1,21 +1,20 @@
 package jp.co.yumemi.android.codecheck.viewmodel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 /** リポジトリ詳細画面のViewModel */
 class RepositoryDetailViewModel : ViewModel() {
-    private val _url = MutableLiveData<String?>()
-    val url: LiveData<String?> get() = _url
+    private val _url = MutableStateFlow<String?>(null)
+    val url: StateFlow<String?> = _url
 
-    /**
-     * URLを開く
-     * @param htmlUrl URL
-     */
     fun openUrl(htmlUrl: String) {
         _url.value = htmlUrl
-        // 画面を回転させたときにURLを開き続けないようにnullをセットする
+    }
+
+    fun clearUrl() {
         _url.value = null
     }
 }
